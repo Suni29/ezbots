6dce4f7dacaaaf1f478d0cf98c5e3d16222c970f
Suni29 <71086259+Suni29@users.noreply.github.com>
2022-01-15 09:33:28 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+class EzBots{
+    constructor(options){
+        this.options = options || {};
+        console.log(`[EzBots] EzBots was installed but its closed alpha, for more information please check out https://ezbots.cf/.`)
+    }
+}
+
+module.exports = EzBots;
